60ee290a718d242eff4e32cb183a1c05720369be
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-11-22 15:45:54 +1100

    OO

--- script.js
+++ script.js
@@ -1,66 +1,21 @@
 "use strict";
-/* chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-
         chrome.scripting.insertCSS({
             target: { tabId: tabId },
-            css: `
-            @keyframes spin {
-                from { transform: rotate(0deg); }
-                to { transform: rotate(360deg); }
-            }
-    
-            @keyframes spin-other {
-                from {
-                    transform: rotate(360deg) scale(0.95);
-                }
-                
-                to {
-                    transform: rotate(0deg) scale(0.95);
-                }
-            }
-    
-            body * {
-                animation: spin-other 10s linear infinite;
-            }
-    
-            .dotcom-virus-heading {
-                font-size: 7.5rem;
-                font-weight: 800;
-            }
-
-            .dotcom-virus-centre {
-                position: absolute;
-                top: 0;
-                left: 0;
-                margin: 1vw;
-                animation: none !important;
-            }
-          `
+            css: "@keyframes spin{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}@keyframes spin-other{from{transform:rotate(360deg)scale(0.95)}to{transform:rotate(0deg) scale(0.95)}}body *{animation:spin-other 10s linear infinite}.dotcom-virus-heading{font-size:7.5rem;font-weight: 800}.dotcom-virus-centre{position:absolute;top:0;left:0;margin:1vw;animation:none !important}"
         });
-        
         chrome.scripting.executeScript({
             target: { tabId: tabId },
-            function: () => {
-                const body: Element | null = document.querySelector("body")
-
-                body != null ? body.innerHTML += "<div class='dotcom-virus-centre'><h1 class='dotcom-virus-heading'>The Dotcom Thing :D</h1></div>" : undefined
-                document.querySelectorAll("body *").forEach(element => {
-                    element.setAttribute("href", "")
-                    element.setAttribute("onsubmit", "")
-                    element.setAttribute("onclick", "")
-                    element.setAttribute("src", "")
-                })
+            func: () => {
+                const body = document.querySelector("body");
+                body != null ? body.innerHTML += "<div class='dotcom-virus-centre'><h1 class='dotcom-virus-heading'>The Dotcom Thing :D</h1></div>" : undefined;
             }
-        })
-
+        });
     }
-})
-
+});
+/*
 body {
     animation: spin 10s linear infinite;
 }
-*/
-chrome.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
-    console.log(document.body.innerHTML);
-});
+*/ 
